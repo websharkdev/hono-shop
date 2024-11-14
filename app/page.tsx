6ed57faf4ad641fca7403335117a392185006d5e
Oleksii <alexey.bortnytskyi@gmail.com>
2024-11-14 20:30:36 +0100

    refactor(app/page) - shows button into app/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
-'use client'
-import { useEffect, useState } from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [message, setMessage] = useState()
+  const [message, setMessage] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch('/api/hello')
-      const { message } = await res.json()
-      setMessage(message)
-    }
-    fetchData()
-  }, [])
+      const res = await fetch("/api/hello");
+      const { message } = await res.json();
+      setMessage(message);
+    };
+    fetchData();
+  }, []);
 
-  if (!message) return <p>Loading...</p>
+  if (!message) return <p>Loading...</p>;
 
-  return <p>{message}</p>
+  return (
+    <p>
+      <Button>Some SHADCN-BUTTON</Button>
+      {message}
+    </p>
+  );
 }
