b003da5153a2e61d546120147ce5d7c6f57605b3
Oleksii <alexey.bortnytskyi@gmail.com>
2024-11-16 17:42:24 +0100

    refactor(app) - improved page structure

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,15 @@
 import { MCards } from './(components)';
 
 const Page = () => {
+  const isLoggined = true;
+
   return (
-    <div>
+    <div className="grid grid-cols-1 gap-2.5">
+      {isLoggined ? (
+        <h5 className="max-w-md truncate text-nowrap text-lg font-semibold">
+          Welcome, Alex!
+        </h5>
+      ) : null}
       <MCards />
     </div>
   );
---
+++ app/products/page.tsx
@@ -0,0 +1,11 @@
+import { Body } from './(components)';
+
+const Page = () => {
+  return (
+    <div>
+      <Body />
+    </div>
+  );
+};
+
+export default Page;
