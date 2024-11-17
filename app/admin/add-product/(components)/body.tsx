fb7c2177a4e56591d55d55a73b719e20afe0746b
Oleksii <alexey.bortnytskyi@gmail.com>
2024-11-17 19:17:21 +0100

    refactor(admin) - renamed route to add-product, created form + connected actions to create product (added to fake products)

---
+++ app/admin/add-product/(components)/body.tsx
@@ -0,0 +1,145 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { CreateProductSchema } from '@/schemas';
+import { z } from 'zod';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import axios from 'axios';
+
+const Body = () => {
+  const form = useForm<z.infer<typeof CreateProductSchema>>({
+    resolver: zodResolver(CreateProductSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+      amount: 0,
+      imageURL: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof CreateProductSchema>) {
+    const response = axios('/api/shop', {
+      method: 'POST',
+      data: values,
+    }).then((result) => {
+      console.log('result', result);
+    });
+
+    console.log('response', response);
+  }
+
+  return (
+    <div className="mx-auto w-full max-w-lg">
+      <Card>
+        <CardHeader>
+          <CardTitle>Let's create new product</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-sm font-semibold text-gray-600 text-primary">
+                      Title
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="Product title" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="imageURL"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-sm font-semibold text-gray-600 text-primary">
+                      Image
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="Image URL" type="url" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-sm font-semibold text-gray-600 text-primary">
+                      Price in $
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="0.00"
+                        type="number"
+                        min={0}
+                        max={500000}
+                        step={0.01}
+                        {...field}
+                        value={form.getValues('amount')}
+                        onChange={(e) =>
+                          form.setValue('amount', Number(e.target.value))
+                        }
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-sm font-semibold text-gray-600 text-primary">
+                      Description
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Tell us a product"
+                        className="resize-none"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" className="w-full text-white">
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Body;
